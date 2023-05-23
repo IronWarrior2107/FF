240947b2dcb199d3e88c5bdd2fd20a69640153f6
IronWarrior2107 <dencool1488xx@gmail.com>
2023-05-23 17:38:45 +0300

    2

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -101,46 +101,6 @@ $(document).ready(function() {
     )});
 });
 
-const slider1 = new Swiper ('.swiper', {
-    speed: 400,
-    spaceBetween: 10,
-    navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
-	},
-    pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-        dynamicBullets: true,
-	},
-    initialSlide: 3,
-    loop: true,
-    autoplay: {
-        delay: 3500,
-        stopOnLastSlide: false,
-        disableOnInteractio: true,
-    }
-});
-
-const slider2 = new Swiper ('.product-swiper', {
-    speed: 500,
-    spaceBetween: 10,
-    navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
-	},
-    simulateTouch: false,
-    initialSlide: 3,
-    slidesPerView: 5,
-    loop: true,
-    autoplay: {
-        delay: 3500,
-        stopOnLastSlide: false,
-        disableOnInteractio: true,
-    }
-})
-
-
 
 // function getRandom(min, max) {
 //     return Math.round(Math.random() * (max - min) + min)
